ad62aefdac8adf8251f3b6351af53054c0075c70
Ciara Gilsenan <ciaragil98@gmail.com>
2022-05-26 16:48:53 +0100

    log change

--- functions/index.js
+++ functions/index.js
@@ -15,29 +15,49 @@ const db = admin.firestore();
 
 app.handle("init", (conv) => {
   // check if they have a user id in user storage if not then most likely a new user
-  // if there is no assigned id, check if the user's email is stored in db, if not, then add new user doc
-  // if email is stored in db, then assign uid to user id
   if (conv.user.params.uid == "" || !conv.user.params.uid) {
     const userEmail = conv.user.params.tokenPayload.email;
+    // check if the user's email is stored in db
     return db.collection("users").where("email", "==", userEmail).get().then((snapshot) => {
       if (snapshot.empty) {
+        // add new user to db
         return db.collection("users").add({
           email: userEmail,
         }).then((docRef) => {
           conv.user.params.uid = docRef.id;
         });
       } else {
+        // assign uid with matching doc
         conv.user.params.uid = snapshot.docs[0].id;
       }
     });
   } else {
     // get most recent analysis
-    return db.collection("users").doc(conv.user.params.uid).collection("analysis").get().then((snapshot) => {
-      // console.log("doc length: ", snapshot.docs.length);
-      if (snapshot.docs.length > 0) {
-        conv.session.params.analysis = snapshot.docs[0].data().data;
-      }
-    });
+    return db.collection("users").doc(conv.user.params.uid).collection("analysis")
+        .orderBy("date", "desc").get().then((snapshot) => {
+          if (snapshot.docs.length > 0) {
+            conv.session.params.analysis = snapshot.docs[0].data().data;
+          }
+        });
+  }
+});
+
+app.handle("checkLog", (conv) => {
+  // get most recent log date from db and compare with today's date
+  const today = admin.firestore.Timestamp.now().toDate();
+  return db.collection("users").doc(conv.user.params.uid).collection("mood_data")
+      .orderBy("date", "desc").get().then((snapshot) => {
+        console.log("Dates: " + today.getDate().toString() + " " +
+          snapshot.docs[0].data().date.toDate().getDate().toString());
+        conv.session.params.hasLoggedToday = today.getDate() == snapshot.docs[0].data().date.toDate().getDate();
+      });
+});
+
+app.handle("initLog", (conv) => {
+  if (conv.session.params.hasLoggedToday) {
+    conv.add("Sorry, you have already logged your mood today. Say exit and come back tomorrow.");
+  } else {
+    conv.add("How was your mood today?");
   }
 });
 
@@ -53,14 +73,13 @@ app.handle("sendDataToDB", (conv) => {
   });
 });
 
-app.handle("checkLog", (conv) => {
-  const today = admin.firestore.Timestamp.now().toDate();
-  return db.collection("users").doc("user1").collection("mood_data")
-      .orderBy("date", "desc").get().then((snapshot) => {
-        console.log("doc: ", snapshot.docs[0].id);
-        console.log("Compare: ", today > snapshot.docs[0].data().date.toDate());
-        conv.session.params.hasLoggedToday = today > snapshot.docs[0].data().date.toDate();
-      });
+app.handle("deliverAnalysis", (conv) => {
+  if (conv.session.params.analysis) {
+    conv.add(conv.session.params.analysis.join(" "));
+  } else {
+    conv.add("Sorry there seems to be a problem with obtaining your analysis.");
+    conv.add("If you are a new user, you will get a new analysis next month.");
+  }
 });
 
 app.handle("sendToGP", (conv) => {
@@ -72,15 +91,6 @@ app.handle("sendToGP", (conv) => {
 
 app.handle("viewLog", (conv) => {
   conv.add("tbd");
-});
-
-app.handle("deliverAnalysis", (conv) => {
-  if (conv.session.params.analysis) {
-    conv.add(conv.session.params.analysis.join(" "));
-  } else {
-    conv.add("Sorry there seems to be a problem with obtaining your analysis.");
-    conv.add("If you are a new user, you will get a new analysis next month.");
-  }
 });
 
 exports.analyseMoodData = functions.pubsub.schedule("0 0 1 * *")
